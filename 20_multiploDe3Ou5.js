8d1eba5e88f5abd87653b3480b2ad9f4be9c1392
Rene Molina de Araujo <rene.araujo@hurst.capital>
2025-04-16 16:20:56 -0300

    20_multiploDe3Ou5

---
+++ 20_multiploDe3Ou5.js
@@ -0,0 +1,19 @@
+/* ### 20. Verificação de Múltiplo de 3 e 5
+**Descrição:** Faça um programa que verifica se um número é múltiplo de 3 e 5 ao mesmo tempo.
+Esses números são frequentemente usados no desafio "FizzBuzz".
+**Objetivos de aprendizado:** Combinar verificações de divisibilidade com operadores lógicos.
+**Dica:** Um número é múltiplo de 3 e 5 se for divisível por 15.
+*/
+
+function input(texto) {
+    process.stdout.write(texto);
+    const { execSync } = require('child_process');
+    return execSync('read input && echo "$input"', { stdio: ['inherit', 'pipe', 'inherit'] })
+        .toString().trim();
+};
+
+let n1 = input("Informe o salario: ");
+let multi = Number (n1);
+
+
+
