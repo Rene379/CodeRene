9897bbade3b809702e120b86684a01bf13af89cc
Rene Molina de Araujo <rene.araujo@hurst.capital>
2025-05-05 17:26:12 -0300

    .

--- lacosRepeticao.js/1_imprimirNumeros.js
+++ lacosRepeticao.js/1_imprimirNumeros.js
@@ -2,3 +2,19 @@
 **Exercício 1:** Faça um programa que imprima os números de 1 a 10.
 */
 
+
+
+
+function input(texto) {
+    process.stdout.write(texto);
+    const { execSync } = require('child_process');
+    return execSync('read input && echo "$input"', { stdio: ['inherit', 'pipe', 'inherit'] })
+        .toString().trim();
+}
+
+for ( i = 0; i <= 10; i++) {
+    console.log (`Os números são: ${i}`)
+};
+
+
+
