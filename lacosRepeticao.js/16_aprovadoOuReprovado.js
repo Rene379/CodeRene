20bab705a6563dfe244bce70e525513e690e9e21
Rene Molina de Araujo <rene.araujo@hurst.capital>
2025-05-13 14:41:31 -0300

    .

---
+++ lacosRepeticao.js/16_aprovadoOuReprovado.js
@@ -0,0 +1,29 @@
+/*
+**Exercício 16:** Crie um programa que leia 10 notas e calcule a média, informando se o aluno está aprovado (média >= 7) ou reprovado.
+*/
+
+function input(texto) {
+    process.stdout.write(texto);
+    const { execSync } = require('child_process');
+    return execSync('read input && echo "$input"', { stdio: ['inherit', 'pipe', 'inherit'] })
+        .toString().trim();
+};
+
+
+let nota = Number();
+let quantasNotas = 1;
+let somaDasnotas = undefined;
+const media = somaDasnotas / quantasNotas;
+
+for (nota = 0; nota < 10; nota++) {
+    let nota = input(`Informe a nota ${quantasNotas}: `)
+    quantasNotas++;
+    somaDasnotas = somaDasnotas + nota;
+    
+};
+
+if (media >= 7 && media <= 10) {
+    console.log("Aprovado! Parabéns!")
+} else {
+    console.log("Reprovado. Na próxima você consegue!")
+};
