c136fcf6c93eba2f781603276ada10f036ebc247
Rene Molina de Araujo <rene.araujo@hurst.capital>
2025-05-22 12:16:51 -0300

    .

---
+++ lacosRepeticao.js/20_serieDeNumeros.js
@@ -0,0 +1,25 @@
+/*
+**Exercício 20:** Faça um programa que mostre os n primeiros termos da série: 1, 3, 5, 7, ...
+*/
+
+
+function input(texto) {
+    process.stdout.write(texto);
+    const { execSync } = require('child_process');
+    return execSync('read input && echo "$input"', { stdio: ['inherit', 'pipe', 'inherit'] })
+        .toString().trim();
+};
+
+
+const q1 = input ("informe a quantidade de repetições: ")
+const repeticoes = Number(q1);
+
+
+for (i = 1; i < repeticoes; i++) {
+   if (i % 2 == 1) {
+    console.log (i)
+    }
+};
+
+
+
