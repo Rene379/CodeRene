18a17db09322b3be3b465289b0877c50ff7b4cde
Rene Molina de Araujo <rene.araujo@hurst.capital>
2025-05-28 17:18:38 -0300

    .

--- lacosRepeticao.js/30_losangoComAsterisco.js
+++ lacosRepeticao.js/30_losangoComAsterisco.js
@@ -21,8 +21,8 @@ function input(texto) {
 };
 
 for (i = 1; i <= 5; i++) {
-    espaco = " ";
-    asterisco = " ";
+    let espaco = " ";
+    let asterisco = " ";
     for (e = 1; e <= 5 - i; e++) {
         espaco += " "
     } for (a = 1; a <= 2 * i - 1; a++) {
@@ -31,8 +31,8 @@ for (i = 1; i <= 5; i++) {
     let cima = (espaco + asterisco);
     console.log(cima)
 } for (i = 4; i >= 1; i--) {
-    let espaco = " ";
-    let asterisco = " ";
+    espaco = " ";
+    asterisco = " ";
 
     for (e = 1; e <= 5 - i; e++) {
         espaco += " "
