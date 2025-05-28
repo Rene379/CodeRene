ffae94e9b5044386ae11172e2c9fc549aa98c900
Rene Molina de Araujo <rene.araujo@hurst.capital>
2025-05-28 17:15:02 -0300

    .

---
+++ lacosRepeticao.js/30_losangoComAsterisco.js
@@ -0,0 +1,44 @@
+/*
+**Exercício 30:** Crie um programa que desenhe um losango simples.
+**Resultado:**
+
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    *
+*/
+
+function input(texto) {
+    process.stdout.write(texto);
+    const { execSync } = require('child_process');
+    return execSync('read input && echo "$input"', { stdio: ['inherit', 'pipe', 'inherit'] })
+        .toString().trim();
+};
+
+for (i = 1; i <= 5; i++) {
+    espaco = " ";
+    asterisco = " ";
+    for (e = 1; e <= 5 - i; e++) {
+        espaco += " "
+    } for (a = 1; a <= 2 * i - 1; a++) {
+        asterisco += "*"
+    }
+    let cima = (espaco + asterisco);
+    console.log(cima)
+} for (i = 4; i >= 1; i--) {
+    let espaco = " ";
+    let asterisco = " ";
+
+    for (e = 1; e <= 5 - i; e++) {
+        espaco += " "
+    } for (a = 1; a <= 2 * i - 1; a++) {
+        asterisco += "*"
+    }
+    let baixo = (espaco + asterisco);
+    console.log(baixo)
+}; 
